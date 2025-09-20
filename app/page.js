b3d49177084cfcfcb0d53a4c65a68c73d7b3bddb
arslanmubarak <138035979+arslanmubarak@users.noreply.github.com>
2025-09-21 03:55:53 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-"use client";
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
@@ -10,24 +9,26 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
 async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`, {
+    next: { revalidate: 3600 }, // optional caching
+  });
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error("Failed to fetch data");
   }
 
   const data = await res.json();
 
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
+  return data
+    .filter((item) => item?.cover_image)
+    .sort(() => Math.random() - 0.5);
+}
 
 export default async function Home() {
   const blogs = await getData();
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -37,5 +38,5 @@ export default async function Home() {
       <Blog blogs={blogs} />
       <ContactSection />
     </div>
-  )
-};
+  );
+}
